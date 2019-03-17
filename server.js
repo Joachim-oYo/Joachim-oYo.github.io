b9ceea118eae72833252c0e19d1a95e311c877a9
Jeremy Joachim <jeremyjoachim17@gmail.com>
2019-03-17 12:58:43 -0400

    Created /clearRestaurants to clean the restaurants.json file

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var restaurantVotes = {};
 function createRestaurant(resto_name) {
     var votes = 1;
     if (resto_name in jsonFile) {
-//        jsonFile[resto_name].votes++;
+        //        jsonFile[resto_name].votes++;
         votes = jsonFile[resto_name].votes + 1;
     }
 
@@ -41,44 +41,36 @@ app.get('/', function (req, res) {
 })
 
 app.post('/addRestaurant', urlencodedParser, function (req, res) {
-    console.log(req.body.restaurant_name);
     jsonFile[req.body.restaurant_name] = createRestaurant(req.body.restaurant_name);
-    //    jsonFile.restaurant4 = restaurant["restaurant4"];
     fs.writeFile(jsonFileName, JSON.stringify(jsonFile, null, 2), function (err) {
         if (err) return console.log(err);
-        //        console.log('writing to ' + jsonFileName);
-        //        data = JSON.parse(data);
-        //        console.log(JSON.stringify(jsonFile, null, 2));
-        res.end(JSON.stringify(jsonFile, null, 2));
+        console.log('Updating ' + req.body.restaurant_name + ' and writing to ' + jsonFileName);
+        console.log(JSON.stringify(jsonFile, null, 2));
+//        res.end(JSON.stringify(jsonFile, null, 2));
     });
-
-    // First read existing users
-    //    fs.readFile(jsonFileName, 'utf8', function (err, data) {
-    //        //
-    //    });
-
 })
 
 app.get('/listRestaurants', function (req, res) {
     fs.readFile(jsonFileName, 'utf8', function (err, data) {
-        data = JSON.parse(data);
+        data = JSON.stringify(JSON.parse(data), null, 2);
         console.log(data);
-        res.end(JSON.stringify(data, null, 2));
+        res.end(data);
     });
 })
 
-app.get('/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile(jsonFileName, 'utf8', function (err, data) {
-        var restaurants = JSON.parse(data);
-        var restaurant = restaurants["restaurant" + req.params.id]
-        console.log(restaurant);
-        res.end(JSON.stringify(restaurant));
+app.get('/clearRestaurants', function (req, res) {
+    jsonFile = {};
+    fs.writeFile(jsonFileName, JSON.stringify(jsonFile, null, 2), function (err) {
+        if (err) return console.log(err);
+        console.log('Clearing JSON file at ' + jsonFileName);
+        res.end('All Restaurants Cleared');
     });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(1243, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("----------------");
+    console.log("Lunch-Voting app listening at http://%s:%s", host, port)
+    console.log("----------------");
 })
